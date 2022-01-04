d40acb3661009164eb8d4122d148162dcfdff438
Brett Seifried <seifriedbrett@gmail.com>
2022-01-04 14:54:10 -0800

    Working DogCard

--- src/Views/HomeDetail.js
+++ src/Views/HomeDetail.js
@@ -8,26 +8,29 @@ import { useParams } from 'react-router-dom';
 export default function Home() {
   const { id } = useParams();
   const [card, setCards] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchDogs(id);
       setCards(data);
+      setLoading(false);
       console.log(data);
     };
     fetchData();
   }, [id]);
 
-  useEffect(() => {
-    getDogsById(id).then(({ data }) => setCards(data));
-  }, [id]);
+  // useEffect(() => {
+  //   getDogsById(id).then(({ data }) => setCards(data));
+  // }, [id]);
+
+  if (loading) return <h1> holder </h1>;
 
   return (
     <div>
-      <DogCard dog={card} />
-      {/* <h2>Test Home Page</h2>
-      <Link to={'/dogs/1'}>Dog</Link>;
-      <DogCard /> */}
+      <ul>
+        <DogCard dog={card} />
+      </ul>
     </div>
   );
 }
--- src/component/DogCard.js
+++ src/component/DogCard.js
@@ -1,39 +1,17 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// export default function dogCard() {
-//   return (
-//     <div>
-//       <h1> dog Name </h1>
-//       <p> Dog Img</p>
-//       <p> dog Detail</p>
-//     </div>
-//   );
-// }
-
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// export default function DogCard({ dog: { id, created_at, name, image, breeds, bio } }) {
-//   return (
-//     <div className="dogCard">
-//       <h2>{name}</h2>
-//       <img src={image} />
-//       <p>{breeds}</p>
-//     </div>
-//   );
-// }
-
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function DogCard({ dog }) {
+  console.log(dog);
+  return (
     <div className="dog-card">
-        {dog.map(({ id, name, image, bio }) => (
-            <h2>{name}</h2>
-            <img src={image} />
-            <p>{bio}</p>
-        ))}
+      {dog.map(({ id, name, image, breed }) => (
+        <Link to={`/dogs/${id}`} key={id}>
+          <h2>{name}</h2>
+          <img alt="dog" src={`${image}`}></img>
+          <p>{breed}</p>
+        </Link>
+      ))}
     </div>
+  );
 }
