5d0ab129150f963c76bda03e69f4ce6aa558d940
Brett Seifried <seifriedbrett@gmail.com>
2022-01-03 16:34:10 -0800

    Console Logging data from API

--- src/Views/HomeDetail.js
+++ src/Views/HomeDetail.js
@@ -1,7 +1,20 @@
 import { Link } from 'react-router-dom';
 import DogCard from '../component/DogCard';
+import { fetchDogs } from '../services/DogRoute';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [card, setCards] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchDogs();
+      setCards(data);
+      console.log(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h2>Test Home Page</h2>
