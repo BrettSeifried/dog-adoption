f192f19b9a9920d142ae62a256d80b2ce140a386
Brett Seifried <seifriedbrett@gmail.com>
2022-01-05 14:36:07 -0800

    Routed to Admin page, created form backend

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import './App.css';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 import Home from './Views/HomeDetail';
 import DogDetail from './Views/DogDetail';
+import Header from './component/Header';
+import DogForm from './Views/Admin';
 
 function App() {
   return (
     <main className="container">
       <BrowserRouter>
-        <header>
-          <NavLink to="/">HOME</NavLink>
-        </header>
+        <Header />
         <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/dogs/:id">
-            <DogDetail />
-          </Route>
+          <Route exact path="/" component={Home} />
+          <Route path="/dogs/admin" component={DogForm} />
+          <Route path="/dogs/:id" component={DogDetail} />
         </Switch>
       </BrowserRouter>
     </main>
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Header() {
-  return <div></div>;
+  return (
+    <div className="header">
+      <NavLink to="/">HOME</NavLink>
+      <NavLink to="/dogs/admin">Manage Pups</NavLink>
+    </div>
+  );
 }
