b5c915baa63a93672b473875ce76c1b94eec7cd9
Brett Seifried <seifriedbrett@gmail.com>
2022-01-07 16:14:18 -0800

    4 Passing snapshot tests

---
+++ src/Views/DogAdmin.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import DogCreate from './DogAdmin';
+import { MemoryRouter } from 'react-router-dom';
+
+test('renders admin page', () => {
+  const { container } = render(
+    <MemoryRouter>
+      <DogCreate />
+    </MemoryRouter>
+  );
+  expect(container).toMatchSnapshot();
+});
